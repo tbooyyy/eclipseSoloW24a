8a09b839940c20a05e2e424a9551446326310495
tbooyyy <bleclai1@emich.edu>
2025-02-03 12:08:24 -0500

    Programmer 2

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
 		System.out.println("Print again");
+		System.out.println("Programmer 2 did this");
 	}
 }
