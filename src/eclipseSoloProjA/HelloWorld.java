c2fc9c9fc8573a87ed61caa5738c7c2d776c425f
tbooyyy <bleclai1@emich.edu>
2025-02-03 11:53:13 -0500

    Did some nifty stuff

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,5 +3,6 @@ package eclipseSoloProjA;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
+		System.out.println("Print again");
 	}
 }
